bd51c94d65248d9cdb5bfb8317987ed7f5211216
Likeur <jacksimba28@gmail.com>
2023-10-27 21:47:37 +0200

    MovieBox update

--- src/app/components/acceuil/acceuil.component.ts
+++ src/app/components/acceuil/acceuil.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import LoginComponent from '../auth/login/login.component';
-import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-acceuil',
   standalone: true,
-  imports: [CommonModule,RouterModule, NavbarComponent],
+  imports: [CommonModule],
   templateUrl: './acceuil.component.html',
   styles: [
   ]
